b89c4d0d03c84068c382dcb6ae8b44287a7c1d4c
Gabriel Sampaio <47998700+Beel132@users.noreply.github.com>
2020-03-27 16:46:16 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-$.getScript('convert.js');
-
 let colors_array = [
     '#4bb543', // success green
     '#F2CB07', // traffic yellow
@@ -40,3 +38,35 @@ color_box_class.on('click', function () {
     let background = rgb2hex($(this).css("background-color"));
     copyToClipboard(background);
 });
+
+// --
+
+// convert rgb color to hex
+function rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+
+function hex(x) {
+    let hexDigits = new Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f");
+    return isNaN(x) ? "00" : hexDigits[(x - x % 16) / 16] + hexDigits[x % 16];
+}
+
+// copy to clipboard
+const copyToClipboard = str => {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    const selected =
+        document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+    if (selected) {
+        document.getSelection().removeAllRanges();
+        document.getSelection().addRange(selected);
+    }
+};
